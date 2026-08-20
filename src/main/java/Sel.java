9ad4fad8934a9705f91f77c74fbb3c327db380da
Selene <seleneltchan@gmail.com>
2026-08-20 15:00:25 +0800

    rename the chatbot to Sel

---
+++ src/main/java/Sel.java
@@ -0,0 +1,10 @@
+public class Sel {
+    public static void main(String[] args) {
+        String banner = " ____  _____ _     \n"
+                      + "/ ___|| ____| |    \n"
+                      + "\\___ \\|  _| | |    \n"
+                      + " ___) | |___| |___ \n"
+                      + "|____/|_____|_____|\n";
+        System.out.println(banner);
+    }
+}
