7729ba8862f3eb6efb3376f60fab6aa8768d10d2
Selene <seleneltchan@gmail.com>
2026-08-20 15:54:15 +0800

    add echo

--- src/main/java/Sel.java
+++ src/main/java/Sel.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Sel {
     public static void main(String[] args) {
         String banner = " ____  _____ _     \n"
@@ -7,7 +9,23 @@ public class Sel {
                       + "|____/|_____|_____|\n";
         System.out.println(banner);
 
+        String line_break = "-----------------------------------------";
         System.out.println("Sup, I'm Sel.");
-        System.out.println("Bye see ya later alligator.");
+        System.out.println(line_break);
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String command = scanner.nextLine();
+            if (command.equals("bye")) {
+                System.out.println(line_break);
+                System.out.println("Bye see ya later alligator.");
+                System.out.println(line_break);
+                break;
+            }
+            System.out.println(command);
+            System.out.println(line_break);
+        }
+        scanner.close();
     }
 }
